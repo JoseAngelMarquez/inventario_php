e4bb4e2909456d72e0d295a13fa73d484b8a7f6f
JoseAngelMarquez <joseangelmarquezespina060503@gmail.com>
2025-07-31 10:34:32 -0600

    Cabia estilos del body

--- views/login.php
+++ views/login.php
@@ -7,23 +7,32 @@
     <meta charset="UTF-8">
 </head>
 <body>
-    <h2>Inicio de Sesión</h2>
-    <form method="POST" action="">
-        <label>Usuario:</label>
-        <input type="text" name="usuario" required><br><br>
+    <div class="container">
+    <div class="right-side">
+            <img src="/asset/img/fondologin.png" alt="logo ueah"">
+        </div>
+        <div class="left-side">
+            
+            <h2>Inicio de Sesión</h2>
+            <form method="POST" action="">
+                <label>Usuario:</label>
+                <input type="text" name="usuario" required><br><br>
 
-        <label>Contraseña:</label>
-        <input type="password" name="contrasena" required><br><br>
+                <label>Contraseña:</label>
+                <input type="password" name="contrasena" required><br><br>
 
-        <button type="submit">Ingresar</button>
-    </form>
+                <button type="submit">Ingresar</button>
+            </form>
 
-    <?php if (!empty($mensaje)) echo "<p>$mensaje</p>"; ?>
+            <?php if (!empty($mensaje)) echo "<p>$mensaje</p>"; ?>
 
-    <?php
-    if (isset($_SESSION['id_usuario'])) {
-        echo "<p>Sesión activa - ID Usuario: " . $_SESSION['id_usuario'] . ", Rol: " . $_SESSION['rol'] . "</p>";
-    }
-    ?>
+            <?php
+            if (isset($_SESSION['id_usuario'])) {
+                echo "<p>Sesión activa - ID Usuario: " . $_SESSION['id_usuario'] . ", Rol: " . $_SESSION['rol'] . "</p>";
+            }
+            ?>
+        </div>
+        
+    </div>
 </body>
 </html>
