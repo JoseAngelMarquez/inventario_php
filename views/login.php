7ef60e0e25a1bd0127ffb7a08e31a6fec1170faa
JoseAngelMarquez <joseangelmarquezespina060503@gmail.com>
2025-07-31 10:26:15 -0600

    Agrega css del login e imagenes a la carpeta

--- views/login.php
+++ views/login.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/asset/login/login.css">
     <title>Login MVC</title>
     <meta charset="UTF-8">
 </head>
