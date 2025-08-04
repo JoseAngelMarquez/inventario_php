ecd0659685ea7ad1762e2baff03d087efaee641c
José Ángel <joseangelmarquezespina060503@gmail.com>
2025-08-04 11:18:17 -0600

    Agrega estilos a la vista de material

--- views/admin/materiales.php
+++ views/admin/materiales.php
@@ -26,6 +26,8 @@ if (isset($_GET['editar'])) {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="/asset/user/prestamo.css">
     <link rel="stylesheet" href="/asset/users/esqueleto.css">
+    <link rel="stylesheet" href="/asset/users/materiales.css">
+
 </head>
 
 <body>
